bf22a46dd572c00fbabaacc8f889141d988e2585
Gabriela Scupin <gabriela.scupin@edu.senai.br>
2026-03-02 08:55:56 -0300

    ADD: Inicio

---
+++ Jogoteca/01_mad_libs/mad_libs.py
@@ -0,0 +1,44 @@
+print("""                               ,---.    ,---.   ____     ______               .---.    .-./`)  _______      .-'''-.  
+                                        |    \  /    | .'  __ `. |    _ `''.           | ,_|    \ .-.')\  ____  \   / _     \ 
+                                        |  ,  \/  ,  |/   '  \  \| _ | ) _  \        ,-./  )    / `-' \| |    \ |  (`' )/`--' 
+                                        |  |\_   /|  ||___|  /  ||( ''_'  ) |        \  '_ '`)   `-'`"`| |____/ / (_ o _).    
+                                        |  _( )_/ |  |   _.-`   || . (_) `. |         > (_)  )   .---. |   _ _ '.  (_,_). '.  
+                                        | (_ o _) |  |.'   _    ||(_    ._) '        (  .  .-'   |   | |  ( ' )  \.---.  \  : 
+                                        |  (_,_)  |  ||  _( )_  ||  (_.\.' /          `-'`-'|___ |   | | (_{;}_) |\    `-'  | 
+                                        |  |      |  |\ (_ o _) /|       .'            |        \|   | |  (_,_)  / \       /  
+                                        '--'      '--' '.(_,_).' '-----'`              `--------`'---' /_______.'   `-...-'   
+
+                                                                                                                                                                                      
+                                                                                     ▄▀                                                                                               
+ ▄▄▄▄ ▄▄▄▄  ▄▄  ▄▄▄  ▄▄  ▄▄ ▄▄▄▄   ▄▄▄     ▄▄▄▄ ▄▄ ▄▄  ▄▄▄    ▄▄ ▄▄ ▄▄  ▄▄▄▄ ▄▄▄▄▄▄ ▄▄▄  ▄▄▄▄  ▄▄  ▄▄▄    ▄▄▄▄  ▄▄▄▄▄ ▄▄▄▄   ▄▄▄▄  ▄▄▄  ▄▄  ▄▄  ▄▄▄  ▄▄    ▄▄ ▄▄▄▄▄  ▄▄▄  ▄▄▄▄   ▄▄▄  
+██▀▀▀ ██▄█▄ ██ ██▀██ ███▄██ ██▀██ ██▀██   ███▄▄ ██ ██ ██▀██   ██▄██ ██ ███▄▄   ██  ██▀██ ██▄█▄ ██ ██▀██   ██▄█▀ ██▄▄  ██▄█▄ ███▄▄ ██▀██ ███▄██ ██▀██ ██    ██   ▄█▀ ██▀██ ██▀██ ██▀██ 
+▀████ ██ ██ ██ ██▀██ ██ ▀██ ████▀ ▀███▀   ▄▄██▀ ▀███▀ ██▀██   ██ ██ ██ ▄▄██▀   ██  ▀███▀ ██ ██ ██ ██▀██   ██    ██▄▄▄ ██ ██ ▄▄██▀ ▀███▀ ██ ▀██ ██▀██ ██▄▄▄ ██ ▄██▄▄ ██▀██ ████▀ ██▀██ """)
+                                                                                                                                                                                      
+
+print("                                      Olá, Um novo mundo espera por você. Entre, explore e deixe com a sua cara.")
+
+#perguntando para o jogador
+print("Vamos lá!✿")
+animal= input("𓅇 Escolha um animal de sua preferência: ")
+lugar= input("⛱︎ Escolha um lugar de sua preferência: ")
+verbo= input("✎ Escolha um verbo de sua preferência: ")
+emocao= input("☺︎ Escolha uma emoçao de sua preferência: ")
+adjetivo= input("❤︎ Escolha um adjetivo de sua preferência: ")
+objeto= input("✂ Escolha um objeto de sua preferência: ")
+ 
+
+#programando 
+
+print (f"""Ninguém sabia exatamente por que aquele(a) {animal} apareceu segurando um(a) {objeto} naquela manhã. Tudo o que se sentia era uma estranha {emocao}, espalhada pelo ar como neblina.
+O lugar era {adjetivo}. Talvez sempre tivesse sido. Talvez só parecesse assim agora, naquele(a) {lugar} onde nada costumava acontecer… até acontecer.
+O(a) {animal} começou a {verbo} lentamente, como se soubesse que estava sendo observado(a). Cada movimento fazia o(a) {objeto} parecer mais importante, mais antigo, quase vivo.
+Quanto mais o tempo passava, mais a {emocao} aumentava. As pessoas sentiam que algo estava prestes a mudar — mas ninguém ousava interromper.
+Então veio a revelação.
+O(a) {animal} parou, olhou diretamente para você… e sorriu.
+Foi nesse momento que você percebeu:
+o(a) {animal} não estava ali por acaso.
+O(a) {objeto} não era dele(a).
+E o(a) {lugar} nunca foi real.
+
+Na verdade, quem estava {verbo} desde o começo…
+era você.""")
